7b588f05d35340f3291c9100969e7b4d69139b91
tishamakhija12 <tishamakhija0@gmail.com>
2026-09-13 16:43:20 +0530

    fix production backend URL

--- 3.interviewIQ/client/src/App.jsx
+++ 3.interviewIQ/client/src/App.jsx
@@ -11,7 +11,8 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://threeinterviewai-back.onrender.com/"
+
+export const ServerUrl  = "https://threeinterviewai-back.onrender.com"
 
 function App() {
 
